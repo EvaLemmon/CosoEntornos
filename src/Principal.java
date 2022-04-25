c887f05795fb1634f94a3b3da42a5f145d9846eb
EvaLemmon <102313528+EvaLemmon@users.noreply.github.com>
2022-04-25 16:32:57 +0200

    Update Principal.java
    pruebas en main

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Usuario user1 = new Usuario("123456789");
+		Usuario user2 = new Usuario("123456788");
 		
+		user1.pszEnviarMensaje(user2);
+		user2.getLoMensajesRecibidos();
 	}
 
 }
