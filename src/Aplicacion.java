7386c9e1a5ec919825a4e2e103bd06da9c9d0062
AdrianCortes <adriancortesrubio@gmail.com>
2022-04-25 17:05:58 +0200

    EnviarMensaje(no acabado)

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -3,5 +3,18 @@ import java.util.ArrayList;
 public class Aplicacion {
 
 	//Atributos
-	ArrayList<Usuario> loUsuarios = new ArrayList<Usuario>();
+	ArrayList<Usuario> loUsuarios;
+	
+	public Aplicacion()
+	{
+		loUsuarios = new ArrayList<Usuario>();
+	}
+
+	public ArrayList<Usuario> getLoUsuarios() {
+		return loUsuarios;
+	}
+
+	public void setLoUsuarios(ArrayList<Usuario> loUsuarios) {
+		this.loUsuarios = loUsuarios;
+	}
 }
--- src/Mensajes.java
+++ src/Mensajes.java
@@ -1,12 +1,29 @@
 import java.util.Objects;
 
 public class Mensajes {
-	private String dueñoMensaje;
-	private String destinatarioMensaje;
 	
 	public Mensajes(String dueñoMensaje, String destinatarioMensaje) {
 		super();
 		this.dueñoMensaje = dueñoMensaje;
 		this.destinatarioMensaje = destinatarioMensaje;
 	}
+	
+	public String getDueñoMensaje() {
+		return dueñoMensaje;
+	}
+
+	public void setDueñoMensaje(String dueñoMensaje) {
+		this.dueñoMensaje = dueñoMensaje;
+	}
+
+	public String getDestinatarioMensaje() {
+		return destinatarioMensaje;
+	}
+
+	public void setDestinatarioMensaje(String destinatarioMensaje) {
+		this.destinatarioMensaje = destinatarioMensaje;
+	}
+
+	private String dueñoMensaje;
+	private String destinatarioMensaje;
 }
