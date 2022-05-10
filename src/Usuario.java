1bfc5a71698deb0954d3980dd676edb9e194c463
AdrianCortes <adriancortesrubio@gmail.com>
2022-05-10 16:23:06 +0200

    Revert "Update Usuario.java"
    This reverts commit 82a187ba0ee491cf500e9ccc79249e4885658fda.

--- src/Usuario.java
+++ src/Usuario.java
@@ -90,17 +90,9 @@ public class Usuario {
 		
 		while(!bSalir)
 		{
-<<<<<<< Updated upstream
 			System.out.println("A quíen quiere inviar un mensaje (teléfono)");
 			szDestinatario = sc.nextLine();
 			
-=======
-			System.out.print("A quíen quiere nviar un mensaje (teléfono): ");
-			szTelefono = sc.nextLine();
-			System.out.println();
-			
-			bCoincide = false;		
->>>>>>> Stashed changes
 			for(int iCont = 0; iCont < oAplicacion.getLoUsuarios().size(); iCont++)
 			{
 				if(oAplicacion.getLoUsuarios().get(iCont).getSzNumTelefono().equals(szDestinatario))
@@ -109,10 +101,6 @@ public class Usuario {
 					bCoincide = true;
 				}
 			}
-<<<<<<< Updated upstream
-=======
-			System.out.println(bCoincide);
->>>>>>> Stashed changes
 			if(bCoincide = false)
 			{
 				System.out.println("Usuario no encontrado");
